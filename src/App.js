de5c4959b52a6a4730e3836adea210b2d7b93d3c
zhao.jing <zhao.jing@dhwork.cn>
2020-07-28 13:44:40 +0800

    create-react-app init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'antd';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="text">less style</span>
+      <Button type='primary'>确认确认</Button>
+    </div>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
+// 主页面
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { message } from 'antd';
-// import { DatePicker, message } from 'antd';
- 
-// 引入css
-import css from '../src/css/main.css';
+import './index.less';
+import App from './App';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: '',
-    };
-  }
-//   handleChange(date) {
-//     message.info('您选择的日期是: ' + date.toString());
-//     this.setState({ date });
-//   }
-    clickEvent(){
-        console.log('点击了我');
-        // message.info('点击了我');
-    }
-  render() {
-    return (
-      <div style={{ width: 400, margin: '100px auto' }} onClick={() => this.clickEvent()}>
-          Hello World!
-        {/* <DatePicker onChange={value => this.handleChange(value)} />
-        <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div> */}
-      </div>
-    );
-  }
-}
- 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
