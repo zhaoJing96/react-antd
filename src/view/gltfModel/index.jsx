3bf58b56ea4a1f375f21213e8e1d8f7d05bbff61
zhao.jing <zhao.jing@dhwork.cn>
2020-12-10 16:20:56 +0800

    组合渲染器渲染选中高亮

--- src/view/gltfModel/index.jsx
+++ src/view/gltfModel/index.jsx
@@ -4,6 +4,9 @@ import { Button } from 'antd';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass';
 // 天空盒子环境贴图图片
 const skyZ1 = require('@/static/image/sky/Sky_DayZ1.png');
 const skyZ2 = require('@/static/image/sky/Sky_DayZ2.png');
@@ -13,16 +16,132 @@ const skyY1 = require('@/static/image/sky/Sky_DayY1.png');
 const skyY2 = require('@/static/image/sky/Sky_DayY2.png');
 const modelUrl = require('@/static/image/JC6BD.glb');
 
+let renderer, scene, camera, composer, outlinePass;
+let isComposer = false; // 是否组合渲染，现实选中高光效果
+let delta = new THREE.Clock().getDelta();//getDelta()方法获得两帧的时间间隔
+
 // three.js加载3D 模型glb
 export default function GltfModelPage() {
-    const [sceneObj, setSceneObj] = useState(null); // 场景对象
-    const [cameraObj, setCameraObj] = useState(null); // 相机对象
-    const [rendererObj, setRendererObj] = useState(null); // 渲染器对象
     const [modelData, setModelData] = useState(null); // 模型对象
     const [modelColorsPosi, setModelColorsPosi] = useState(null); // 用于存储模型颜色位置
     const box = useRef(); // canvas盒子
     console.log(modelColorsPosi);
-    // 改变颜色
+    // 设置天空盒子背景
+    function setSceneBackground() {
+        // 创建一个环境、使用环境贴图
+        let cubeTextureLoader = new THREE.CubeTextureLoader();
+        let urls = [
+            skyX1,
+            skyX2,
+            skyY1,
+            skyY2,
+            skyZ1,
+            skyZ2
+        ];
+        let cubeTexture = cubeTextureLoader.load(urls);
+        // 利用环境贴图设置场景背景
+        scene.background = cubeTexture;
+    }
+    // 设置灯光
+    function setLight() {
+        let light = new THREE.AmbientLight(0xffffff);
+        scene.add(light);
+        // 平行光
+        let directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(-4, 8, 4);
+        scene.add(directionalLight);
+        let directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight1.position.set(-4, 8, -4);
+        scene.add(directionalLight1);
+        // 光源开启阴影
+        directionalLight.castShadow = true;
+        directionalLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+        directionalLight.shadow.bias = -0.001;
+    }
+    // 加载模型、存储模型初始颜色等参数
+    function setGltfModel() {
+        // 导入GlTF模型
+        let gltfLoader = new GLTFLoader();
+        gltfLoader.load(modelUrl, (gltf) => {
+            gltf.scene.traverse(obj => {
+                // 模型Mesh开启阴影
+                if (obj.isMesh) {
+                    obj.castShadow = true;
+                    obj.receiveShadow = true;
+                }
+                // 存储模型小零件初始颜色、位置、名称
+                let modelObj = null;
+                let modelArr = [];
+                if (obj.isMesh) {
+                    modelObj = {
+                        name: obj.name,
+                        color: obj.material.color,
+                        posiX: obj.position.x,
+                        posiY: obj.position.y,
+                        posiZ: obj.position.z
+                    }
+                }
+                modelArr.push(modelObj);
+                setModelColorsPosi(modelArr);
+            });
+            setModelData(gltf.scene);
+            scene.add(gltf.scene);
+        });
+    }
+    // 获取与射线相交的对象数组
+    function getCanvasIntersects(event) {
+        // 声明 raycaster 和 mouse 变量
+        let rayCaster = new THREE.Raycaster();
+        let mouse = new THREE.Vector2();
+        event.preventDefault();
+        // 判断canvas画布是否是整个屏幕
+        if (box.current) {
+            // box.current画布所在的div，通过整个scene所在的容器来界定的
+            let getBoundingClientRect = box.current.getBoundingClientRect(); // 方法返回元素的大小及其相对于视口的位置
+            mouse.x = ((event.clientX - getBoundingClientRect.left) / box.current.offsetWidth) * 2 - 1;
+            mouse.y = -((event.clientY - getBoundingClientRect.top) / box.current.offsetHeight) * 2 + 1;
+        } else {
+            // 通过鼠标点击位置,计算出 raycaster 所需点的位置,以屏幕为中心点,范围 -1 到 1
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        }
+        //通过鼠标点击的位置(二维坐标)和当前相机的矩阵计算出射线位置
+        rayCaster.setFromCamera(mouse, camera);
+        // 返回射线选中的对象 第二个参数如果不填 默认是false，设置为true检测所有后代
+        let intersects = rayCaster.intersectObjects(scene.children, true);
+        //返回选中的对象数组
+        return intersects;
+    }
+    // 设置模型高亮选中
+    function setComposer(width, height) {
+        // 设置高亮
+        composer = new EffectComposer(renderer); // 配置composer
+        let renderPass = new RenderPass(scene, camera); // 配置通道
+        composer.addPass(renderPass); // 将通道加入composer
+        outlinePass = new OutlinePass(new THREE.Vector2(width, height), scene, camera);
+        outlinePass.visibleEdgeColor.set('#fff000'); // 选中颜色
+        outlinePass.edgeStrength = 5; // 强度
+        outlinePass.edgeGlow = 1.5; // 边缘明暗度
+        outlinePass.renderToScreen = true; // 设置这个参数的目的是马上将当前的内容输出
+        composer.addPass(outlinePass);
+        composer.selectedObjectEffect = function (objs) {
+            let selectedObjects = [];
+            selectedObjects.push(objs);
+            outlinePass.selectedObjects = selectedObjects;
+        }
+    }
+    // 渲染动画
+    function renderFn() {
+        requestAnimationFrame(renderFn);
+        if (isComposer) {
+            // 组合渲染器，渲染高亮
+            composer.render(delta);
+        } else {
+            // 用相机渲染一个场景
+            renderer.render(scene, camera);
+        }
+    }
+    // 指定模型子集改变颜色
     function changeColor(e) {
         let target = e.target || e.srcElement;
         if (target.nodeName.toLowerCase() === 'li') {
@@ -54,153 +173,69 @@ export default function GltfModelPage() {
             }
         }
     }
-    // 监听窗体变化
+
+    // 监听窗体变化、自适应窗体事件
     function onWindowResize() {
         let width = box.current.offsetWidth;
         let height = box.current.offsetHeight;
-        cameraObj.left = width / - 2;
-        cameraObj.right = width / 2;
-        cameraObj.top = height / 2;
-        cameraObj.bottom = height / -2;
+        camera.left = width / - 2;
+        camera.right = width / 2;
+        camera.top = height / 2;
+        camera.bottom = height / -2;
         // 更新相机投影矩阵，在相机任何参数被改变以后必须被调用
-        cameraObj.updateProjectionMatrix();
-        rendererObj.setSize(width, height);
+        camera.updateProjectionMatrix();
+        renderer.setSize(width, height);
     }
-    // 获取与射线相交的对象数组
-    function getCanvasIntersects(event) {
-        // 声明 raycaster 和 mouse 变量
-        let rayCaster = new THREE.Raycaster();
-        let mouse = new THREE.Vector2();
-        event.preventDefault();
-        // 判断canvas画布是否是整个屏幕
-        if (box.current) {
-            // box.current画布所在的div，通过整个scene所在的容器来界定的
-            let getBoundingClientRect = box.current.getBoundingClientRect(); // 方法返回元素的大小及其相对于视口的位置
-            mouse.x = ((event.clientX - getBoundingClientRect.left) / box.current.offsetWidth) * 2 - 1;
-            mouse.y = -((event.clientY - getBoundingClientRect.top) / box.current.offsetHeight) * 2 + 1;
-        } else {
-            // 通过鼠标点击位置,计算出 raycaster 所需点的位置,以屏幕为中心点,范围 -1 到 1
-            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-        }
-        //通过鼠标点击的位置(二维坐标)和当前相机的矩阵计算出射线位置
-        rayCaster.setFromCamera(mouse, cameraObj);
-        // 返回射线选中的对象 第二个参数如果不填 默认是false，设置为true检测所有后代
-        let intersects = rayCaster.intersectObjects(sceneObj.children, true);
-        //返回选中的对象数组
-        return intersects;
-    }
+    // 监听事件 窗体监听、点击事件监听
     useEffect(() => {
-        if (sceneObj) {
-            // 导入GlTF模型
-            let gltfLoader = new GLTFLoader();
-            gltfLoader.load(modelUrl, (gltf) => {
-                gltf.scene.traverse(obj => {
-                    // 模型Mesh开启阴影
-                    if (obj.isMesh) {
-                        obj.castShadow = true;
-                        obj.receiveShadow = true;
-                    }
-                    // 存储模型小零件初始颜色、位置、名称
-                    let modelObj = null;
-                    let modelArr = [];
-                    if (obj.isMesh) {
-                        modelObj = {
-                            name: obj.name,
-                            color: obj.material.color,
-                            posiX: obj.position.x,
-                            posiY: obj.position.y,
-                            posiZ: obj.position.z
-                        }
-                    }
-                    modelArr.push(modelObj);
-                    setModelColorsPosi(modelArr);
-                });
-                setModelData(gltf.scene);
-                sceneObj.add(gltf.scene);
-            });
-        }
-    }, [sceneObj]);
+        // 监听窗体变化
+        window.addEventListener('resize', onWindowResize, false);
+        // 监听点击事件
+        box.current.addEventListener('click', (event) => {
+            let selectObj = getCanvasIntersects(event);
+            if (selectObj && selectObj.length > 0) {
+                isComposer = true;
+                composer.selectedObjectEffect(selectObj[0].object);
+            } else {
+                isComposer = false;
+            }
+        });
+    }, []);
 
+    // 初始化环境、灯光、相机、渲染器
     useEffect(() => {
-        if (cameraObj && rendererObj) {
-            // 监听窗体变化
-            window.addEventListener('resize', onWindowResize, false);
-
-            // 监听点击事件
-            box.current.addEventListener('click', (event) => {
-                let selectObj = getCanvasIntersects(event);
-                console.log(selectObj);
-                if (selectObj.length > 0) {
-
-                }
-            });
-        }
-    }, [cameraObj, rendererObj]);
-
-    useEffect(() => {
-        let scene = new THREE.Scene();
-        // 创建一个环境、使用环境贴图
-        let cubeTextureLoader = new THREE.CubeTextureLoader();
-        let urls = [
-            skyX1,
-            skyX2,
-            skyY1,
-            skyY2,
-            skyZ1,
-            skyZ2
-        ];
-        let cubeTexture = cubeTextureLoader.load(urls);
-        // 利用环境贴图设置场景背景
-        scene.background = cubeTexture;
-        setSceneObj(scene);
-
+        scene = new THREE.Scene();
+        // 天空盒子
+        setSceneBackground();
         // 添加光源
-        let light = new THREE.AmbientLight(0xffffff);
-        scene.add(light);
-        // 平行光
-        let directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(-4, 8, 4);
-        scene.add(directionalLight);
-        let directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight1.position.set(-4, 8, -4);
-        scene.add(directionalLight1);
-        // 光源开启阴影
-        directionalLight.castShadow = true;
-        directionalLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-        directionalLight.shadow.bias = -0.001;
-
+        setLight();
+        // 加载模型
+        setGltfModel();
         // 获取宽高设置相机和渲染区域大小
         let width = box.current.offsetWidth;
         let height = box.current.offsetHeight;
         let k = width / height;
         // 投影相机
-        let camera = new THREE.PerspectiveCamera(60, k, 0.1, 3000);
+        camera = new THREE.PerspectiveCamera(60, k, 0.1, 3000);
         camera.position.set(250, 500, 700);
         camera.lookAt(scene.position);
-        setCameraObj(camera);
 
         // 创建一个webGL对象
-        let renderer = new THREE.WebGLRenderer();
+        renderer = new THREE.WebGLRenderer();
         renderer.setSize(width, height); // 设置渲染区域尺寸
         renderer.setClearColor(0xffffff, 1); // 设置颜色透明度
         // 首先渲染器开启阴影
         renderer.shadowMap.enabled = true;
         box.current.appendChild(renderer.domElement);
-        setRendererObj(renderer);
-
+        // 高亮设置
+        setComposer(width, height);
         // 渲染
-        function renderFn() {
-            // 用相机渲染一个场景
-            renderer.render(scene, camera);
-            requestAnimationFrame(renderFn);
-        }
         renderFn();
-
         // 监听鼠标事件
         let controls = new OrbitControls(camera, renderer.domElement);
         controls.addEventListener('change', renderFn);
     }, []);
+
     return <div className='ui_container_box'>
         three.js加载3D模型
         <div className='ui_model_container'>
